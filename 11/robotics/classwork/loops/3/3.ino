83b2cc7648d4bbcc92376046c4066dd7aa79a2b2
Ingo Wolf <me@ingowolf.au>
2026-02-09 10:36:04 +1100

    add v3

---
+++ 11/robotics/classwork/loops/3/3.ino
@@ -0,0 +1,23 @@
+const int delayTime = 50;
+
+void setup() {
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  for (int x = 8; x < 14; x++) {
+    digitalWrite(x, HIGH);
+    delay(delayTime);
+    digitalWrite(x, LOW);
+  }
+  for (int x = 13; x > 7; x--) {
+    digitalWrite(x, HIGH);
+    delay(delayTime);
+    digitalWrite(x, LOW);
+  }
+}
