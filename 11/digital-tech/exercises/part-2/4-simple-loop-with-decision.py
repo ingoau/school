0dbb10a400293c7518172d849481dba04f4d20a7
Ingo Wolf <me@ingowolf.au>
2026-02-09 12:43:55 +1100

    add is even function

---
+++ 11/digital-tech/exercises/part-2/4-simple-loop-with-decision.py
@@ -0,0 +1,6 @@
+# Use a for loop to print the numbers from 1 to 20.
+# Print &quot;Even&quot; next to even numbers and &quot;Odd&quot; next to odd numbers.
+
+
+def isEven(number):
+    return number % 2 == 0
